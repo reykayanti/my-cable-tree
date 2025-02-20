5123b59e3a1cd6c97ec4809e1474858480fb0994
Retno Ekayanti <retnoekayanti15@gmail.com>
2025-02-20 16:45:10 +0700

    s

---
+++ src/components/Promotion/Promotion.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Promotion.css'
+
+const Promotion = () => {
+  return (
+    <div className='promotion'>
+      <p data-aos="fade-up" data-aos-duration="1000">Made with love by me. Get in touch if you'd like to design your own custom link-on-bio page</p>
+      <div className="ll" data-aos="fade-up" data-aos-duration="2000"></div>
+      <div className="tq" data-aos="fade-up" data-aos-duration="1000">Thank You</div>
+    </div>
+  )
+}
+
+export default Promotion
