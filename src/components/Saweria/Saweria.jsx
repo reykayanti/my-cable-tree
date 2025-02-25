5d4706ac1b8e350665df864a8ade4ea3cb8d10bd
Retno Ekayanti <retnoekayanti15@gmail.com>
2025-02-25 10:18:27 +0700

    update links

--- src/components/Saweria/Saweria.jsx
+++ src/components/Saweria/Saweria.jsx
@@ -3,6 +3,11 @@ import './Saweria.css'
 import { assets } from '../../assets/assets'
 
 const Saweria = () => {
+
+    const handleClick = () => {
+        window.open("https://saweria.co/reykayanti", '_blank', 'noopener,noreferrer')
+      }
+
   return (
     <div className='saweria'>
         <div className="ico">
@@ -17,7 +22,7 @@ const Saweria = () => {
         </div>
         <div className="cta">
             <p data-aos="fade-up" data-aos-duration="1000">If you'd like, you can support me with a cup of coffee</p>
-            <div className="to-saweria" data-aos="fade-up" data-aos-duration="1500">
+            <div className="to-saweria" data-aos="fade-up" data-aos-duration="1500" onClick={handleClick}>
                 <img src={assets.saweria} alt="" />
                 Saweria
             </div>
--- src/components/Tiktok/Tiktok.jsx
+++ src/components/Tiktok/Tiktok.jsx
@@ -3,14 +3,19 @@ import './Tiktok.css'
 import { assets } from '../../assets/assets'
 
 const Tiktok = () => {
+
+  const handleClick = () => {
+    window.open("https://www.tiktok.com/@reykayanti", '_blank', 'noopener,noreferrer')
+  }
+
   return (
     <div className='tiktok'>
         <div className="capt">
             <p data-aos="fade-up" data-aos-duration="1200" >Got<br/>CODING<br/>on your<br/>mind?</p>
             <div className='checkout' data-aos="fade-up" data-aos-duration="1300" >Check out here</div>
-            <img className='arrow-capt' src={assets.arrow2} alt="" data-aos="fade-up" data-aos-duration="1700"/>
+            <img className='arrow-capt' onClick={handleClick} src={assets.arrow2} alt="" data-aos="fade-up" data-aos-duration="1700"/>
         </div>
-        <div className="tiktok-preview" data-aos="fade-up" data-aos-duration="1000">
+        <div className="tiktok-preview" data-aos="fade-up" data-aos-duration="1000" onClick={handleClick}>
             <img src={assets.tiktokimg} alt="" />
         </div>
     </div>
